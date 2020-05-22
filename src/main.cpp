e628bbc8b58959c637f6e0667e2194ba74e1899c
Sebastian <sebulba@tatooine.home>
2020-05-22 22:10:11 +0200

    Pierwszy kod dla UNO - obsluga wyswietlacza

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,45 @@
 #include <Arduino.h>
 
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void blink();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  Serial.println("Start"); //Napis kontrolny startu programu
+  //Zabawa wyświetlaczem
+  lcd.begin(16, 2);
+  lcd.home();
+  lcd.print("Dziala");
 }
+
+int x = 6;
+int y = 4;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  blink();
+  // blink(); // uruchomienie migania diody
+  for (int licznik = 0; licznik < 10; licznik++)
+  {
+    Serial.println("Dodawanie: ");
+    Serial.println(x + y);
+    Serial.println(licznik);
+    Serial.println();
+  delay(2000);
+  }
+  
 }
 
+// definicja funkcji migania diody na PCB Uno
 void blink() {
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(100);
-Serial.println("loop completed"); 
+// Serial.println("loop completed"); // przy pierwszym uruchomieniu wypisywałem komunikat kończący pętlę
 }
