cbcb7bf7218d00d44020d81a2b18cf3b4dd12d8f
Sebastian <sebulba@tatooine.home>
2020-05-26 15:02:27 +0200

    Add DHT22 measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h>  // biblioteka odpowiedzialna za LCD
+#include <DHT.h>            // biblioteka odpowiedzialna za DHT
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#define DHTPIN 8         // pin do korego jestem przylaczony
+#define DHTTYPE DHT22     // typ czujnika
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;   // piny do których podłączony jest wyświetlacz
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);                    // tworzę obiekt klasy LiquidCrystal o powyzszych paramatrach
+
+DHT dht(DHTPIN, DHTTYPE); // tworzę obiekt klasy DHT o nazwie dht o podanych parametrach
 
 void blink();
 
@@ -11,19 +17,50 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Start"); //Napis kontrolny startu programu
-  //Zabawa wyświetlaczem
-  lcd.begin(16, 2);
+  Serial.println("Start pomiaru");  //Napis kontrolny startu programu
+  
+  dht.begin();        // inicjalizacja czujnika
+  lcd.begin(16, 2);   // inicjalizacja LCD
+  
   lcd.home();
-  lcd.print("Dziala");
+  lcd.print("Pomiar ..."); 
 }
 
 int x = 6;
 int y = 4;
 
 void loop() {
-  // put your main code here, to run repeatedly:
   // blink(); // uruchomienie migania diody
+  delay(2000);  //oczekiwanie na pomiar przez czujnik - normalnie zajmuje ok. 250 msek do nawet 2 sek
+  float temp = dht.readTemperature(); // tworzę zmienną typu float przechowującą pomiar temperatury
+  float wilg = dht.readHumidity(); // tworzę zmienną typu float przechowującą pomiar wilgotności
+
+  if (isnan(temp) || isnan(wilg)) { // sprawdzam czy nie ma błędu odczytu
+    lcd.home(); // kursor na 0, 0
+    lcd.print("Blad odczytu"); // wypisuję napis na lcd
+    return; // wychodzę z pętli
+  }
+  
+  // wyswietlam dane na LCD
+  lcd.home(); // kursor na 0, 0
+  lcd.print("T: ");
+  lcd.print(temp); // wypisuję zmienną temp
+  lcd.print(" stC");
+  lcd.setCursor(0, 1); // kursor na drugi wiersz
+  lcd.print("W: ");
+  lcd.print(wilg); // wypisuję zmienną wilg
+  lcd.print("  %");
+ 
+  // wyswitlam dane na terminalu
+  Serial.print("T: ");
+  Serial.print(temp);
+  Serial.println(" stC");
+  Serial.print("W: ");
+  Serial.print(wilg);
+  Serial.println(" %");
+
+
+  /*
   for (int licznik = 0; licznik < 10; licznik++)
   {
     Serial.println("Dodawanie: ");
@@ -32,7 +69,7 @@ void loop() {
     Serial.println();
   delay(2000);
   }
-  
+  */
 }
 
 // definicja funkcji migania diody na PCB Uno
